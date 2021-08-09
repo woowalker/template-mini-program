d3c1d7f065cef961be7c75e3e477da006adb160b
xiongqiang <qxiong@optikom.cn>
2021-08-09 16:36:46 +0800

    chore: 项目配置项

--- plugins/api/config.js
+++ plugins/api/config.js
@@ -2,6 +2,7 @@ export default {
   // 请求 host
   baseUrl: 'http://192.168.2.121:8800',
   socketUrl: 'ws://192.168.2.121:3241/wss',
+  socketUrl3242: 'ws://192.168.2.121:3242/wss',
   // 修正路径，可用这个控制接口版本号，比如 /apiv2
   prefixPath: '/api',
   // this.$api['common/getUserInfo'] 中的 common/getUserInfo 分隔符，分割命名空间和接口
