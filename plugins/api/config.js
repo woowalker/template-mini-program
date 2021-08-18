91f1ec9b22e31c244684c03372b41e70b0c0fb15
xiongqiang <qxiong@optikom.cn>
2021-08-18 11:31:57 +0800

    feat: https 配置

--- plugins/api/config.js
+++ plugins/api/config.js
@@ -1,8 +1,8 @@
 export default {
   // 请求 host
-  baseUrl: 'https://cms.optiems.com',
-  socketUrl: 'ws://cms.optiems.com/wss',
-  socketUrl3242: 'ws://cms.optiems.com/wsm',
+  baseUrl: 'https://www.optiems.com',
+  socketUrl: 'ws://www.optiems.com/wss',
+  socketUrl3242: 'ws://www.optiems.com/wsm',
   // 修正路径，可用这个控制接口版本号，比如 /apiv2
   prefixPath: '/api',
   // this.$api['common/getUserInfo'] 中的 common/getUserInfo 分隔符，分割命名空间和接口
